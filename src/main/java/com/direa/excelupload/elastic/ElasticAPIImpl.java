dfa29366348c078de30c49739bf124fa48513ec4
geniebro <genielvup@gmail.com>
2020-01-14 18:14:30 +0900

    엘라스틱 api 구현클래스 등록

---
+++ src/main/java/com/direa/excelupload/elastic/ElasticAPIImpl.java
@@ -0,0 +1,23 @@
+package com.direa.excelupload.elastic;
+
+import java.util.List;
+
+import com.direa.excelupload.CustomException;
+import com.direa.excelupload.vo.ClientcallVO;
+import com.direa.excelupload.vo.CommuteVO;
+
+public class ElasticAPIImpl implements ElasticAPI {
+
+	@Override
+	public boolean putCommuteIndex(List<CommuteVO> list) throws CustomException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean putClientcallIndex(ClientcallVO vo) throws CustomException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
